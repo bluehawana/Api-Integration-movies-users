20fd967d7ac8ab7d550fa4f251ce732b57a8f405
Lars Strömberg <lars@dsve.se>
2024-05-28 10:12:30 +0000

    Add LoginUserDto.java

---
+++ src/main/java/se/dsve/movies/dtos/LoginUserDto.java
@@ -0,0 +1,12 @@
+package se.dsve.movies.dtos;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LoginUserDto {
+    private String email;
+    private String password;
+}
