a78c07fb59d8361abdd46d3327544d18b7b7e74d
Lars Strömberg <lars@dsve.se>
2024-05-28 10:12:31 +0000

    Add ApplicationConfiguration.java

---
+++ src/main/java/se/dsve/movies/configs/ApplicationConfiguration.java
@@ -0,0 +1,46 @@
+package se.dsve.movies.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import se.dsve.movies.repository.UserRepository;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    private final UserRepository userRepository;
+
+    public ApplicationConfiguration(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        // TODO: Implement function
+        return null;
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        // TODO: Implement function
+        return null;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        // TODO: Implement function
+        return null;
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        // TODO: Implement function
+        return null;
+    }
+}
