ee4385db1628a2a8e6c3a42f590875610d11d4f2
Hongzhi Li <bluehawana@gmail.com>
2024-05-31 16:15:22 +0200

    modify authenticationservice.java to original 12

--- src/main/java/se/dsve/movies/MoviesApplication.java
+++ src/main/java/se/dsve/movies/MoviesApplication.java
@@ -2,12 +2,16 @@ package se.dsve.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import se.dsve.movies.services.MoviesService;
 
 @SpringBootApplication
 public class MoviesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoviesApplication.class, args);
-	}
+ public static void main(String[] args) {
+  ApplicationContext context = SpringApplication.run(MoviesApplication.class, args);
+  MoviesService moviesService = context.getBean(MoviesService.class);
+  moviesService.addPredefinedMovies();
+ }
 
 }
