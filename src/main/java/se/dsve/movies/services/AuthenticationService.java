c9cfb45d93059b0de3f8f66d9761968b52480c02
Hongzhi Li <bluehawana@gmail.com>
2024-06-06 09:40:23 +0200

    modify authenticationservice.java and removing jwtservice

--- src/main/java/se/dsve/movies/services/AuthenticationService.java
+++ src/main/java/se/dsve/movies/services/AuthenticationService.java
@@ -2,55 +2,41 @@ package se.dsve.movies.services;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import se.dsve.movies.dtos.LoginUserDto;
 import se.dsve.movies.dtos.RegisterUserDto;
-import se.dsve.movies.model.LoginResponse;
 import se.dsve.movies.model.User;
 import se.dsve.movies.repository.UserRepository;
-import java.util.NoSuchElementException;
-
 
 @Service
 public class AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
-    private final JwtService jwtService;
-
 
     public AuthenticationService(
             UserRepository userRepository,
             AuthenticationManager authenticationManager,
-            PasswordEncoder passwordEncoder,
-            JwtService jwtService
+            PasswordEncoder passwordEncoder
     ) {
         this.userRepository = userRepository;
         this.authenticationManager = authenticationManager;
         this.passwordEncoder = passwordEncoder;
-        this.jwtService = jwtService;
     }
 
     public User signup(RegisterUserDto input) {
-
         User user = new User();
         user.setEmail(input.getEmail());
         user.setPassword(passwordEncoder.encode(input.getPassword()));
         return userRepository.save(user);
     }
 
-
     public User authenticate(LoginUserDto input) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(input.getUsername(), input.getPassword())
         );
-        User user = userRepository.findByEmail(input.getUsername())
-                .orElseThrow(() -> new NoSuchElementException("User not found"));
-
-        String token = jwtService.generateToken(user);
-        long expiresIn = jwtService.getExpirationTime();
-        return user;
+        return userRepository.findByEmail(input.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
     }
 }
